7d266c5cc63853385e06b4cbb2907d00b7f507ae
lateefay <hammeda582@gmail.com>
2025-12-06 05:42:43 +0100

    i edited all the website and inserted social links

---
+++ index.js
@@ -0,0 +1,10 @@
+//  mobile navition show and hide toggle
+
+function show() {
+    document.querySelector(".mobile-links").style.display = "block"
+}
+
+function hide() {
+    document.querySelector(".mobile-links").style.display = "none"
+}
+

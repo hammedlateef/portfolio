d23ee19d55113815778a819b131ea1b65420bec5
lateefay <hammeda582@gmail.com>
2025-12-10 14:08:24 +0100

    reconstructed again and changed logo

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 //  mobile navition show and hide toggle
 
-function show() {
-    document.querySelector(".mobile-links").style.display = "block"
-}
+let showNav = document.querySelector(".show-nav");
+let hideNav = document.querySelector(".fa-xmark");
 
-function hide() {
+showNav.addEventListener("click", () => {
+    document.querySelector(".mobile-links").style.display = "block"
+})
+
+hideNav.addEventListener("click", () => {
     document.querySelector(".mobile-links").style.display = "none"
-}
+})
+
+// document.querySelectorAll(".close-nav-with-body-click").addEventListener("click", () => {
+//     document.querySelector(".mobile-links").style.display = "none";
+//     console.log("i clicked hero");
+
+// })
 
